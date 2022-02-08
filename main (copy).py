0ff7b62f35fc7e1e30127c47fd51456b20eb2c9d
EvelynBrooks1 <98491379+Evelynb92@users.noreply.github.com>
2022-02-08 18:00:10 +0000

    Initial commit

---
+++ main (copy).py
@@ -0,0 +1,8 @@
+print('Hi everyone, please enter your grade number to find out what letter grade you received.')
+
+A= int(input('Enter your grade:'))
+if A>=90:
+  print('You have recevied a', 'A')
+
+elif mar>=80 and B<90:
+  print('You have recevied a', 'B')
---
+++ main.py
@@ -0,0 +1,13 @@
+print('Hi everyone, please enter your grade number to find out what letter grade you received. \n')
+
+mar=int(input('Enter your grade here:'))
+if mar>=90:
+  print('You have received a A')
+elif mar>=80 and mar<90:
+  print('You have received a B')
+elif mar>=70 and mar<80:
+  print('You have received a C')
+elif mar>=70 and mar<60:
+  print('You have received a D')
+elif mar<=60:
+  print('You have received a F')
